64847f99d4d8204c27765e296e6c94b739830845
Salim Mayan <mailsalim@gmail.com>
2021-08-25 20:07:42 -0700

    fix KegList (MVP - can display keg name in keg List)

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Keg(props) {
     return (
         <React.Fragment>
-            <h1>keg</h1>
+            <h1>{props.onKegName}</h1>
         </React.Fragment>
     );
+}
+
+Keg.propTypes = {
+    onKegName: PropTypes.string
 }
 
 export default Keg;
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Keg from './Keg';
+import PropTypes from 'prop-types';
 
 function KegList(props) {
     return (
         <React.Fragment>
-            <Keg />
+            {props.kegList.map((keg) => {
+                return <Keg onKegName={keg.kegName} />
+            })}
         </React.Fragment>
     );
+}
+
+KegList.propTypes = {
+    kegList: PropTypes.object
 }
 
 export default KegList;
