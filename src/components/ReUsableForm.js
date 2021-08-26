0a37d32c211315538fda14574f6f84dac53e5d30
Salim Mayan <mailsalim@gmail.com>
2021-08-25 19:06:59 -0700

    add Reusable form which will be used by both NewKegForm and EditKegForm

--- src/components/ReUsableForm.js
+++ src/components/ReUsableForm.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ReUsableForm (props) {
-    return(
-        <React.Fragment>
-<form>
-    
-</form>
+function ReUsableForm(props) {
+    return (
+        <React.Fragment onSubmit={props.resuableFormSubmissionHandler}>
+            <form >
+                <input type='text' name='kegName' placeholder='Enter Name' />
+                <input type='text' name='kegBrand' placeholder='Enter Brand' />
+                <input type='text' name='kegPrice' placeholder='Enter Price' />
+                <input type='text' name='kegFlavor' placeholder='Enter Keg Flavor' />
+                <button type='submit'>{props.buttonText}</button>
+            </form>
         </React.Fragment>
     );
+
+    ReUsableForm.propTypes = {
+        buttonText: PropTypes.string,
+        resuableFormSubmissionHandler: PropTypes.func
+    };
+
 }
 
 export default ReUsableForm;
