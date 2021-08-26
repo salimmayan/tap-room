cb84b3491bd4201abcb03a4a589177247c39998d
Salim Mayan <mailsalim@gmail.com>
2021-08-25 19:29:34 -0700

    add NewKegForm which shares ReUsableForm component with EditKegForm

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import ReUsableForm from './ReUsableForm';
 
 function EditKegForm(props) {
-    return(
+
+    function handleEditKegFormSubmission (event) {
+        event.preventDefault();
+        props.onEditKegForm({kegName: event.target.kegName.value, kegBrand: event.target.kegBrand.value, kegPrice: event.target.kegPrice.value, kegFlavor: event.target.kegFlavor.value});
+    }
+
+    return (
         <React.Fragment>
-<form>
-    
-</form>
+            <ReUsableForm resuableFormSubmissionHandler ={props.handleEditKegFormSubmission} buttonText = "Edit Keg" />
         </React.Fragment>
     );
+
+    EditKegForm.propTypes = {
+        onEditKegForm: propTypes.func
+    }
 }
 
 export default EditKegForm;
--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import ReUsableForm from './ReUsableForm';
 
 function NewKegForm(props) {
+
+    function handleNewKegFormSubmission (event) {
+        event.preventDefault();
+        props.onNewKegForm({kegName: event.target.kegName.value, kegBrand: event.target.kegBrand.value, kegPrice: event.target.kegPrice.value, kegFlavor: event.target.kegFlavor.value});
+    }
+
     return (
         <React.Fragment>
-            <h1>NewKegForm</h1>
+           <ReUsableForm resuableFormSubmissionHandler ={props.handleNewKegFormSubmission} buttonText = "Add Keg" />
         </React.Fragment>
     );
+
+    NewKegForm.propTypes = {
+        onNewKegForm: propTypes.func
+    }
 }
 
-export default NewKegForm;
+export default EditNewKegFormKegForm;
