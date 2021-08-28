3adf7dcaac88b9859fb6726bd451149d84b725b2
Salim Mayan <mailsalim@gmail.com>
2021-08-28 02:06:03 -0700

    fix bug where qty will disappear after editing keg details

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -6,7 +6,7 @@ function EditKegForm(props) {
     function handleEditKegFormSubmission(event) {
         event.preventDefault();
         const kegId = props.keg.kegId;
-        props.onEditKegForm({ kegName: event.target.kegName.value, kegBrand: event.target.kegBrand.value, kegPrice: event.target.kegPrice.value, kegFlavor: event.target.kegFlavor.value, kegQty: props.keg.kegQty.value, kegId: kegId });
+        props.onEditKegForm({ kegName: event.target.kegName.value, kegBrand: event.target.kegBrand.value, kegPrice: event.target.kegPrice.value, kegFlavor: event.target.kegFlavor.value, kegQty: props.keg.kegQty, kegId: kegId });
     }
 
     return (
